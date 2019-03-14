39595e1ee417dcdbf5ffd29b97b6c5646cc34e2a
Naveen Kulkarni <naveenkulkarni029@gmail.com>
2019-03-14 10:35:03 +0530

    added collection Implementation

--- src/org/nbk/algorithms/searching/BinarySearching.java
+++ src/org/nbk/algorithms/searching/BinarySearching.java
@@ -1,24 +1,25 @@
 package org.nbk.algorithms.searching;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class BinarySearching {
 
 	
 	public static void main(String[] args) {
 		int arr[] = { 2, 3, 4, 10, 40 }; 
-		int key = 30;
+		int key = 10;
 		int indexRecursive = binarySearchRecursive(arr,0, arr.length-1, key);
 		int indexLoop = binarySearchLoop(arr, 0, arr.length-1, key);
-		if(indexRecursive==-1) {
-			System.out.println("not found Recursive");
-		}else {
-			System.out.println("found in indexRecursive "+indexRecursive);
-		}
+		int indexCollection = binarySearchCollection(arr, key);
+		int indexArrays = binarySearchArray(arr, key);
 		
-		if(indexLoop==-1) {
-			System.out.println("not found indexLoop");
-		}else {
-			System.out.println("found in indexLoop "+indexLoop);
-		}
+		printIndex(indexRecursive);
+		printIndex(indexLoop);
+		printIndex(indexCollection);
+		printIndex(indexArrays);
 	}
 
 	private static int binarySearchRecursive(int[] arr, int low, int high, int key) {
@@ -48,5 +49,27 @@ public class BinarySearching {
 			low = mid+1;
 		}
 		return -1;
+	}
+	
+	private static int binarySearchCollection(int[] arr, int key) {
+		List<Integer> intCollection = new ArrayList<>();
+		for(int i=0; i<=arr.length-1;i++) {
+			intCollection.add(arr[i]);
+		}
+		Collections.sort(intCollection);
+		return Collections.binarySearch(intCollection, key);
+	}
+	
+	private static int binarySearchArray(int[] arr, int key) {
+		Arrays.sort(arr);
+		return Arrays.binarySearch(arr, key);
+	}
+	
+	private static void printIndex(int index) {
+		if(index==-1) {
+			System.out.println("not found");
+		}else {
+			System.out.println("found in index "+index);
+		}
 	}
 }
